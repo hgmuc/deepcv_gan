adbd2ee5c45f3d9c7d967682d07fc91ac3b73cbe
hgmuc <48514702+hgmuc@users.noreply.github.com>
2019-03-13 13:08:36 +0100

    Update utils.py
    Fix in cropped image

--- utils.py
+++ utils.py
@@ -78,7 +78,10 @@ def crop_and_resize(image, image_shape, resize_height=64, resize_width=64, crop=
     else:
         cropped_image = resize(image, [resize_height, resize_width])
         
-    return np.array(cropped_image)/127.5 - 1.
+    if np.max(cropped_images <= 1):
+        return np.array(cropped_image)/0.5 - 1.
+    else:   
+        return np.array(cropped_image)/127.5 - 1.
 
 def print_output(f, msg):
     print(msg)
